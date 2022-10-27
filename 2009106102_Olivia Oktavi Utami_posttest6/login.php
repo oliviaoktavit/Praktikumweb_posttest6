c41899010b0ada147c9193a37fa5e03814e602eb
oyipiy <102163458+oliviaoktavit@users.noreply.github.com>
2022-10-27 21:57:05 +0800

    posttest 6

---
+++ 2009106102_Olivia Oktavi Utami_posttest6/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIGN UP</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+
+<body>
+    <div class="kotak-signup">
+        <p class="Text-login">SIGN UP</p>
+        <div class="lg">
+            <button class="form-signup"> <a href="signup.php">Customer</a></button> <br>
+        </div>
+        <div>
+            <button class="form-signup"> <a href="db.php">Admin</a></button>
+        </div>
+    </div>
+</body>
+
+</html>
